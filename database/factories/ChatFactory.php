00629010c7d2fab481a70982c84f5bb94c9c0c7f
Yudho Alfantyo <nosvengeance@gmail.com>
2020-09-15 22:52:50 +0700

    initial commit

---
+++ database/factories/ChatFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\{Chat, User};
+use Faker\Generator as Faker;
+
+$factory->define(Chat::class, function (Faker $faker) {
+    return [
+        'subject' => $faker->sentence(5),
+        'user_id' => User::all()->random()->id,
+    ];
+});
